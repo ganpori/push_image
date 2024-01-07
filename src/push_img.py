52b70a8490c2d4a65540d48f581785e1735abe9a
TaikiIwahori <hmyrwjj@yahoo.co.jp>
2024-01-08 03:51:01 +0900

    ENH: jpg送信した

--- src/push_img.py
+++ src/push_img.py
@@ -2,7 +2,7 @@ import paramiko
 import os
 
 from dotenv import load_dotenv
-
+from pathlib import Path
 
 
 
@@ -13,15 +13,17 @@ if __name__=="__main__":
     user=os.environ.get("User")
     path_key=os.environ.get("IdentityFile")
 
-    local_path="/tmp_camera/images"
-    remote_path="/tmp/camera_images"
+    local_path="/tmp/camera_images/"
+    remote_path="/tmp/camera_images/"
     with paramiko.SSHClient() as sshc:
         sshc.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         sshc.connect(hostname=ip, port=port,username=user,key_filename=path_key)
         with sshc.open_sftp() as sftp:
             try:
-                sftp.chdir(remote_path)
-            except FileNotFoundError:
                 sftp.mkdir(path=remote_path)
-                sftp.chdir(remote_path)
+            except OSError:
+                pass
             
+            for path_jpg in Path(local_path).glob("*.jpg"):
+                path_str_remote_jpg=remote_path+path_jpg.name 
+                sftp.put(localpath=path_jpg, remotepath=path_str_remote_jpg)
