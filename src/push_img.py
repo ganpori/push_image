28d580fc8f15e671fd45f46576d12b3c57afbe2c
TaikiIwahori <hmyrwjj@yahoo.co.jp>
2024-01-08 03:18:07 +0900

    ENH: リモートのディレクトリ作れるようにした

--- src/push_img.py
+++ src/push_img.py
@@ -13,6 +13,16 @@ if __name__=="__main__":
     port=os.environ.get("Port")
     user=os.environ.get("User")
     path_key=os.environ.get("IdentityFile")
+
+    local_path="/tmp_camera/images"
+    remote_path="/tmp/camera_images"
     with paramiko.SSHClient() as sshc:
         sshc.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         sshc.connect(hostname=ip, port=port,username=user,key_filename=path_key)
+        with sshc.open_sftp() as sftp:
+            try:
+                sftp.chdir(remote_path)
+            except FileNotFoundError:
+                sftp.mkdir(path=remote_path)
+                sftp.chdir(remote_path)
+            
