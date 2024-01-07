e0903e1235845dd18979b6a6cbaad552f15f2d13
TaikiIwahori <hmyrwjj@yahoo.co.jp>
2024-01-08 01:46:47 +0900

    ENH: paramikoでssh接続できるようにした

--- src/push_img.py
+++ src/push_img.py
@@ -2,6 +2,7 @@ import paramiko
 import os
 
 from dotenv import load_dotenv
+import scp
 
 
 
@@ -9,4 +10,9 @@ from dotenv import load_dotenv
 if __name__=="__main__":
     load_dotenv()
     ip=os.environ.get("HostName")
-    print(ip)
+    port=os.environ.get("Port")
+    user=os.environ.get("User")
+    path_key=os.environ.get("IdentityFile")
+    with paramiko.SSHClient() as sshc:
+        sshc.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        sshc.connect(hostname=ip, port=port,username=user,key_filename=path_key)
